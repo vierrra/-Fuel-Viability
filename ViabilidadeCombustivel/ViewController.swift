496167b38d0582b444017bd53dc57b3afcab7d96
Renato <renato.vieira@stant.com.br>
2021-01-16 22:11:25 -0300

    Initial Commit

---
+++ ViabilidadeCombustivel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViabilidadeCombustivel
+//
+//  Created by Renato on 1/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
